35f2bbb896b61969ff01d122a1ad6732d82f0541
Brice Lambson <brice@bricelam.net>
2021-09-25 17:50:34 -0700

    Convert to YAML

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const gulp = require("gulp");
+const js_yaml = require("js-yaml");
+const plist = require("plist");
+
+gulp.task("default", done => {
+    fs.writeFileSync(
+        "T4Language/Syntaxes/t4.tmLanguage",
+        plist.build(js_yaml.load(fs.readFileSync("T4Language/Syntaxes/t4.tmLanguage.yml"))));
+    fs.writeFileSync(
+        "T4Language/Syntaxes/t4.tmTheme",
+        plist.build(js_yaml.load(fs.readFileSync("T4Language/Syntaxes/t4.tmTheme.yml"))));
+    done();
+});
