3aa1f0bcdd4fbd1f9afc7be5d33449906913c171
Brice Lambson <brice@bricelam.net>
2021-10-12 15:28:08 -0700

    Brute-force correct parsing by editing the C# language

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,14 @@
+/// <binding BeforeBuild='default' />
+
 const fs = require("fs");
 const gulp = require("gulp");
 const js_yaml = require("js-yaml");
 const plist = require("plist");
 
 gulp.task("default", done => {
+    fs.writeFileSync(
+        "T4Language/Syntaxes/csharp.tmLanguage",
+        plist.build(js_yaml.load(fs.readFileSync("T4Language/Syntaxes/csharp.tmLanguage.yml"))));
     fs.writeFileSync(
         "T4Language/Syntaxes/t4.tmLanguage",
         plist.build(js_yaml.load(fs.readFileSync("T4Language/Syntaxes/t4.tmLanguage.yml"))));
